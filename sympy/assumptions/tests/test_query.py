cbbf8502901ce4f25bb5e7b6107e8a37c239c517
Jisoo Song <jeesoo9595@snu.ac.kr>
2020-12-24 14:34:55 +0900

    Add test

--- sympy/assumptions/tests/test_query.py
+++ sympy/assumptions/tests/test_query.py
@@ -2291,14 +2291,39 @@ def test_custom_AskHandler():
     def _(expr, assumptions):
         from sympy import log
         if ask(Q.integer(log(expr + 1, 2))):
-                return True
+            return True
     @Q.mersenne.register(Symbol)
     def _(expr, assumptions):
         if expr in conjuncts(assumptions):
-                return True
+            return True
     assert ask(Q.mersenne(7))
     assert ask(Q.mersenne(n), Q.mersenne(n))
     del Q.mersenne
+
+
+def test_polyadic_predicate():
+
+    class SexyPredicate(Predicate):
+        pass
+    Q.sexyprime = SexyPredicate("sexyprime")
+
+    @Q.sexyprime.register(Integer, Integer)
+    def _(int1, int2, assumptions):
+        args = sorted([int1, int2])
+        if not all(ask(Q.prime(a), assumptions) for a in args):
+            return False
+        return args[1] - args[0] == 6
+
+    @Q.sexyprime.register(Integer, Integer, Integer)
+    def _(int1, int2, int3, assumptions):
+        args = sorted([int1, int2, int3])
+        if not all(ask(Q.prime(a), assumptions) for a in args):
+            return False
+        return args[2] - args[1] == 6 and args[1] - args[0] == 6
+
+    assert ask(Q.sexyprime(5, 11))
+    assert ask(Q.sexyprime(7, 13, 19))
+    del Q.sexyprime
 
 
 def test_Predicate_handler_is_unique():
