a496aee2ed12f70409d25d1a855e4fb6d4fb9648
Jisoo Song <jeesoo9595@snu.ac.kr>
2021-04-15 12:56:48 +0900

    feat: introduce symbolic equation
    Introduce SideProxy

--- sympy/equation/equation.py
+++ sympy/equation/equation.py
@@ -2,6 +2,8 @@
 from sympy.core import Basic
 from sympy.core.sympify import _sympify
 
+from .sideproxy import SideProxy
+
 
 class SymbolicRelation(Basic):
 
@@ -14,11 +16,28 @@ class SymbolicRelation(Basic):
 
     @property
     def lhs(self):
+        """The left-hand side of the relation."""
         return self.args[0]
 
     @property
     def rhs(self):
+        """The right-hand side of the relation."""
         return self.args[1]
+
+    @property
+    def apply(self):
+        """Proxy object to apply operation on both sides."""
+        return SideProxy(self, "both")
+
+    @property
+    def applylhs(self):
+        """Proxy object to apply operation on left hand sides."""
+        return SideProxy(self, "lhs")
+
+    @property
+    def applyrhs(self):
+        """Proxy object to apply operation on right hand sides."""
+        return SideProxy(self, "rhs")
 
 
 class Equation(SymbolicRelation):
